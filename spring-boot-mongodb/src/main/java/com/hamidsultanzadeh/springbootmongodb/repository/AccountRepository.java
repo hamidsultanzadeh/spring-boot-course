39b18d984a545a7d6220143ab092516506e18e85
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-06-19 18:11:41 +0400

    delete old mongo-example

--- spring-boot-mongodb/src/main/java/com/hamidsultanzadeh/springbootmongodb/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hamidsultanzadeh.springbootmongodb.repository;
-
-import com.hamidsultanzadeh.springbootmongodb.entity.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends MongoRepository<Account,String> {
-}
